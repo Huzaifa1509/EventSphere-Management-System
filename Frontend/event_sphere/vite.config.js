7afc843fa15a6032dd83d7f8814bdf476161b910
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-11-30 00:47:12 +0500

    Tailwind + Shadcn Installed In The Frontend.

--- Frontend/event_sphere/vite.config.js
+++ Frontend/event_sphere/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// Define __dirname in an ESM environment
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Use __dirname here
+    },
+  },
+});
